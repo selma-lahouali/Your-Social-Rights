d45d5b4c7542dbc97c0d21b2cacf806c6915195e
selma-lahouali <lahoualiselma@gmail.com>
2026-03-11 04:38:41 +0100

    style*

--- src/Pages copy/DisabilityInsurance/DisabilityInsurance.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function DisabilityInsurance() {
-  return (
-    <div>DisabilityInsurance</div>
-  )
-}
--- src/Pages copy/OccupationalDiseases/OccupationalDiseases.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function OccupationalDiseases() {
-  return (
-    <div>OccupationalDiseases</div>
-  )
-}
