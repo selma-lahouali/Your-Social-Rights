5f187d64346384115cd148a2d07abba0a1276010
selma-lahouali <lahoualiselma@gmail.com>
2026-03-25 22:20:39 +0100

    semantic+accessibility

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import CNRlogo from "../../assets/CNR_logo.png";
@@ -6,32 +5,42 @@ import CNASlogo from "../../assets/CNAS_logo.png";
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <div id="nabvar">
-      <div id="navbarLogo">
-        <Link to="/">
-          <img src={logo} alt="your social rights logo" />
-          <h1>Vos droit sociaux</h1>
-        </Link>
-      </div>
-      <div id="navbarLinks">
-        <ul id="navbarList">
-          <li>
-            <Link to="downloads"> Télécharments</Link>
-          </li>
-          <li>
-            <Link to="suggestions">Vos suggestions</Link>
-          </li>
-          <li>
-            <Link to="claims"> Vos réclamations</Link>
-          </li>
-        </ul>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <img id="CNR" src={CNRlogo} alt="CNR logo" />
-        </a>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          <img id="CNAS" src={CNASlogo} alt="CNAS logo" />
-        </a>
-      </div>
-    </div>
+    <header>
+      <nav id="navbar">
+        <div id="navbarLogo" aria-label="Navigation principale">
+          <Link to="/">
+            <img src={logo} alt="Vos droit sociaux logo" />
+            <h1>Vos droit sociaux</h1>
+          </Link>
+        </div>
+        <div id="navbarLinks">
+          <ul id="navbarList">
+            <li>
+              <Link to="downloads"> Télécharments</Link>
+            </li>
+            <li>
+              <Link to="suggestions">Vos suggestions</Link>
+            </li>
+            <li>
+              <Link to="claims"> Vos réclamations</Link>
+            </li>
+          </ul>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img
+              id="CNR"
+              src={CNRlogo}
+              alt="logo CNR pour acceder au site officiel"
+            />
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <img
+              id="CNAS"
+              src={CNASlogo}
+              alt="logo CNAS pour acceder au site officiel"
+            />
+          </a>
+        </div>
+      </nav>
+    </header>
   );
 }
