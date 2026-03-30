b460d74e18e69f3c77f3767c4ddda2e6924afde3
selma-lahouali <lahoualiselma@gmail.com>
2026-03-30 22:51:39 +0100

    layout.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -41,41 +41,6 @@ export default function Navbar() {
           </a>
         </div>
       </nav>
-      <nav id="navbar">
-        <div id="navbarLogo" aria-label="Navigation principale">
-          <Link to="/">
-            <img src={logo} alt="Vos droit sociaux logo" />
-            <h1>Vos droit sociaux</h1>
-          </Link>
-        </div>
-        <div id="navbarLinks">
-          <ul id="navbarList">
-            <li>
-              <Link to="downloads"> Télécharments</Link>
-            </li>
-            <li>
-              <Link to="suggestions">Vos suggestions</Link>
-            </li>
-            <li>
-              <Link to="claims"> Vos réclamations</Link>
-            </li>
-          </ul>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            <img
-              id="CNR"
-              src={CNRlogo}
-              alt="logo CNR pour acceder au site officiel"
-            />
-          </a>
-          <a href="http://" target="_blank" rel="noopener noreferrer">
-            <img
-              id="CNAS"
-              src={CNASlogo}
-              alt="logo CNAS pour acceder au site officiel"
-            />
-          </a>
-        </div>
-      </nav>
     </header>
   );
 }
