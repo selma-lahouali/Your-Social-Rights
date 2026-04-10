7ccc0e1e0d5986202e6eeeb58c90a7e5d082d49f
selma-lahouali <lahoualiselma@gmail.com>
2026-04-10 19:26:11 +0100

    side3.

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -24,6 +24,366 @@ export default function Sidebar() {
           <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
         </li>
       </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
+      <ul id="sidebarList">
+        <li>
+          <NavLink to="healthInsurance">Assurance Maladie</NavLink>
+        </li>
+        <li>
+          <NavLink to="maternityInsurance">Assurance Maternité</NavLink>
+        </li>
+        <li>
+          <NavLink to="disabilityInsurance">Assurance Invalidité</NavLink>
+        </li>
+        <li>
+          <NavLink to="familyAllocation">Allocation Familliales</NavLink>
+        </li>
+        <li>
+          <NavLink to="workAccident">Accidents de Travail</NavLink>
+        </li>
+        <li>
+          <NavLink to="occupationalDiseases">Maladies Professionnelles</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
